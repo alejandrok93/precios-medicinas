f10ff21e9b250cabba329ff8b06e7058983497d8
alejandrok93 <alejandrok93@gmail.com>
2020-05-24 14:37:41 -0700

    added delayed loop for making requests

--- medicine-descriptions.js
+++ medicine-descriptions.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var _ = require('lodash');
 // const request = require('request');
 var cheerio = require('cheerio');
 const axios = require('axios');
@@ -13,53 +14,82 @@ let raw_urls = fs.readFileSync('medicine-urls.json');
 let urls = JSON.parse(raw_urls);
 
 // urls.forEach(item => makeRequest(item.url));
-for (let i = 0; i < 1; i++) {
+
+let index = 0;
+for (let i = 600; i < urls.length; i++) {
 	const { url } = urls[i];
-	console.log(url);
-	makeRequest(url);
+	setTimeout(makeRequest, index * 2000, url);
+	index++;
 }
+
+// const rawHTMLFile = fs.readFileSync(
+// 	'medicines-sample-html-content.txt',
+// 	'utf8'
+// );
+// const htmlArray = rawHTMLFile.split(';;;;;;;;;;');
+// htmlArray.forEach(html => getInfo(cheerio.load(html)));
+
+// if (medicines.length > 100) {
+// 	fs.writeFileSync(
+// 		'medicines-with-sku-and-description-final.json',
+// 		JSON.stringify(medicines)
+// 	);
+// }
 
 function makeRequest(url) {
 	//Make HTTP request to product URL
+	console.log(url);
 	var options = {
 		headers: {
 			'User-Agent':
 				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 			'Cache-Control': 'no-cache',
 			cookie:
-				'LAST_CATEGORY=880; CATEGORY_INFO=%7B%22limit%22%3A%2256%3Fp%3D1%22%7D; visid_incap_2194663=VQTICNsTS0i+lKet7uRAxFEUyF4AAAAAQUIPAAAAAACedj9cTJ7WKWLIRb6XH6RD; nlbi_2194663=xUTbPUzfGEvgLC6IGayX4AAAAAAdB3KfROttwkAmRVMiqVz3; _gcl_au=1.1.1902052630.1590170712; _ga=GA1.2.301313865.1590170712; _hjid=971e49ec-30d5-4e60-b32d-0d07d8e9b3f8; CACHED_FRONT_FORM_KEY=6cqz4a8PBN1VqzHC; _gid=GA1.2.1669434573.1590288549; frontend=u8ol50s7ch3ste7g6a6kpiqeu3; incap_ses_1057_2194663=dv0rcwPUnjGh6FRQ9DerDk+yyl4AAAAAuhe3Z2RkobDioJHnlr3gRw==; external_no_cache=1; VIEWED_PRODUCT_IDS=625%2C626%2C9926; _hjAbsoluteSessionInProgress=1; frontend_cid=ALlFp4kVxMRFvH8D; __atuvc=2%7C21%2C4%7C22; __atuvs=5ecab251d1384ff2003; _gat_gtag_UA_43421096_11=1; _gat_UA-43421096-4=1'
-		}
+				'CACHED_FRONT_FORM_KEY=agC1Gb4yidjVoGOb; VIEWED_PRODUCT_IDS=6084; frontend=595f32h4rf10e7kgiid4ufrfc2; frontend_cid=lWRORfEoHxci3gFo; visid_incap_2194663=7OG5Up0iQIuQ2VAPY6lUNuLjyl4AAAAAQUIPAAAAAADT/kgKHzJuUqWb5mF4riOE; incap_ses_1057_2194663=7M4aAuNpyArCNlhQ9DerDuLjyl4AAAAAQUyXrVVIHnzT77ESU4cDKQ==; external_no_cache=1; __atuvc=1%7C22; __atuvs=5ecae3e401efd73f000; _gcl_au=1.1.711215386.1590354917; _ga=GA1.2.2051195664.1590354917; _gid=GA1.2.511871333.1590354917; _gat_gtag_UA_43421096_11=1; _gat_UA-43421096-4=1; _hjid=c008cb8a-2a7d-4286-8c67-9e6429dd1484; _fbp=fb.1.1590354917604.1910570275; _hjAbsoluteSessionInProgress=1',
+		},
 	};
-
 	axios
 		.get(url, options)
-		.then(response => {
+		.then((response) => {
 			if (response.status === 200) {
 				const html = response.data;
+				writeHtmlContentToFile(html);
 				const $ = cheerio.load(html);
 				getInfo($, url);
 			} else {
 				console.log('ERROR making request');
 			}
 		})
-		.catch(error => console.log('ERROR making request', error.message));
+		.catch((error) => console.log('ERROR making request', error.message));
+
+	//Sample html for dev
+	// const html = fs.readFileSync('medicines-sample-html-content.txt');
+	// const $ = cheerio.load(html, url);
+	// getInfo($);
 }
 
 function getInfo($, url) {
-	let name = $('div.page-title')
+	let name = $('div.page-title').text().trim();
+
+	let price = $('span.price').text().trim();
+
+	let sku = $('#product-attribute-specs-table > tbody > tr:nth-child(2) > td')
 		.text()
 		.trim();
 
-	let price = $('span.price')
-		.text()
-		.trim();
-	let sku = $('tbody')
-		.find('td')
-		.first()
+	let caracteristicas = $(
+		'#product-attribute-specs-table > tbody > tr:nth-child(3) > td'
+	)
 		.text()
 		.trim();
 
-	const description = $('div.std')
+	let ingredientes = $(
+		'#product-attribute-specs-table > tbody > tr:nth-child(4) > td'
+	)
+		.text()
+		.trim();
+
+	const description = $('#product_tabs_description_contents > div > div')
 		.text()
 		.trim();
 
@@ -68,10 +98,11 @@ function getInfo($, url) {
 		name: name,
 		price: price,
 		sku: sku,
+		caracteristicas: caracteristicas,
+		ingredientes: ingredientes,
 		description: description,
-		url: url
+		url: url,
 	};
-	console.log({ item });
 	medicines.push(item);
 	console.log('Number of medicines: ' + medicines.length);
 	writeMedicinesFile(item);
@@ -83,9 +114,19 @@ function writeMedicinesFile(medicine) {
 	}
 
 	fs.appendFile(
-		'medicines-with-sku-description.txt',
+		'medicines-with-sku-and-description.json',
 		JSON.stringify(medicine),
-		err => {
+		(err) => {
+			if (err) throw err;
+		}
+	);
+}
+
+function writeHtmlContentToFile(html) {
+	fs.appendFile(
+		'medicines-sample-html-content.txt',
+		html.concat(';;;;;;;;;;'),
+		(err) => {
 			if (err) throw err;
 		}
 	);
