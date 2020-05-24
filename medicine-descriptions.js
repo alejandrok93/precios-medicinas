b779fd2e315bec56de5a9ffedbb17bc44fbc20f9
alejandrok93 <alejandrok93@gmail.com>
2020-05-24 15:04:43 -0700

    updated with more logging

--- medicine-descriptions.js
+++ medicine-descriptions.js
@@ -16,6 +16,8 @@ let urls = JSON.parse(raw_urls);
 // urls.forEach(item => makeRequest(item.url));
 
 let index = 0;
+let success = 0;
+console.log('Total URLs:', urls.length);
 for (let i = 600; i < urls.length; i++) {
 	const { url } = urls[i];
 	setTimeout(makeRequest, index * 2000, url);
@@ -29,12 +31,12 @@ for (let i = 600; i < urls.length; i++) {
 // const htmlArray = rawHTMLFile.split(';;;;;;;;;;');
 // htmlArray.forEach(html => getInfo(cheerio.load(html)));
 
-// if (medicines.length > 100) {
-// 	fs.writeFileSync(
-// 		'medicines-with-sku-and-description-final.json',
-// 		JSON.stringify(medicines)
-// 	);
-// }
+if (medicines.length > 100) {
+	fs.writeFileSync(
+		'medicines-with-sku-and-description-final.json',
+		JSON.stringify(medicines)
+	);
+}
 
 function makeRequest(url) {
 	//Make HTTP request to product URL
@@ -45,8 +47,9 @@ function makeRequest(url) {
 				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 			'Cache-Control': 'no-cache',
 			cookie:
-				'CACHED_FRONT_FORM_KEY=agC1Gb4yidjVoGOb; VIEWED_PRODUCT_IDS=6084; frontend=595f32h4rf10e7kgiid4ufrfc2; frontend_cid=lWRORfEoHxci3gFo; visid_incap_2194663=7OG5Up0iQIuQ2VAPY6lUNuLjyl4AAAAAQUIPAAAAAADT/kgKHzJuUqWb5mF4riOE; incap_ses_1057_2194663=7M4aAuNpyArCNlhQ9DerDuLjyl4AAAAAQUyXrVVIHnzT77ESU4cDKQ==; external_no_cache=1; __atuvc=1%7C22; __atuvs=5ecae3e401efd73f000; _gcl_au=1.1.711215386.1590354917; _ga=GA1.2.2051195664.1590354917; _gid=GA1.2.511871333.1590354917; _gat_gtag_UA_43421096_11=1; _gat_UA-43421096-4=1; _hjid=c008cb8a-2a7d-4286-8c67-9e6429dd1484; _fbp=fb.1.1590354917604.1910570275; _hjAbsoluteSessionInProgress=1',
+				'CACHED_FRONT_FORM_KEY=DknvLBY6WKagKzAr; frontend=660q7fp2ac6vua4bf6b7snnme4; frontend_cid=4F2P6uLFq2Vm0ph1; visid_incap_2194663=nYE5vN08S6iDvbGQ01uP6Gvryl4AAAAAQUIPAAAAAADgLvTYGBynZwDekeDz6QNp; incap_ses_1057_2194663=wHPaLq5MRVwioFhQ9DerDmvryl4AAAAAW4jMHvluI5/OEs1itHgb6A==; _ga=GA1.2.62365311.1590356846; _gid=GA1.2.704241259.1590356846; _gcl_au=1.1.452559511.1590356846; _hjid=729933c6-9278-41fd-9343-d7e6f73b06a1; _fbp=fb.1.1590356846157.1459255338; VIEWED_PRODUCT_IDS=1782; external_no_cache=1; __atuvc=2%7C22; __atuvs=5ecaebb59ee4b146001',
 		},
+		withCredentials: true,
 	};
 	axios
 		.get(url, options)
@@ -56,6 +59,8 @@ function makeRequest(url) {
 				writeHtmlContentToFile(html);
 				const $ = cheerio.load(html);
 				getInfo($, url);
+				console.log('Number of successful requests', success);
+				success++;
 			} else {
 				console.log('ERROR making request');
 			}
